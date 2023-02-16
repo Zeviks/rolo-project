fdc6943908f4df65e175fe3e3895a643f743ed5f
Natanael Pacheco <natepacheco716@gmail.com>
2023-02-16 03:12:00 -0600

    Completed search logic.

--- rolo-dex/src/App.tsx
+++ rolo-dex/src/App.tsx
@@ -1,17 +1,43 @@
-import { useState, useRef } from "react"
+import { useState, useEffect } from "react"
+
+interface userState {
+    name: string,
+    id: number,
+}
 
 const App = ():JSX.Element => {
-let [name, setName] = useState("Nathan");
 
-  return (
-    <div className="">
-        <h1>Hello my name is {name}.</h1>
-        <input type="text" className="mt-2" />
-        <button className="pl-2" onClick={() => {
-            setName("NewName");
-        }}>Change Name</button>
-    </div>
-  )
+    const API_URL = "https://jsonplaceholder.typicode.com/users";
+
+    
+    let [monsters, setMonsters] = useState<userState[]>([]) 
+    let [searchString, setSearchString] = useState("");
+    
+    const filteredMonsters = monsters.filter((monster) => monster.name.toLocaleLowerCase().includes(searchString));
+     
+    const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const searchString = e.target.value.toLocaleLowerCase();
+        setSearchString(searchString);
+    }
+
+    useEffect(() => {
+        fetch(API_URL)
+        .then((res) => res.json())
+        .then((users) => setMonsters(users))
+    }, []) // Add empty dependency array here, this tells react to only run this once.
+  
+    return (
+        <div className="">
+          
+            <input 
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            type="search" 
+            placeholder="search Monsters" 
+            onChange={(e) => onSearchChange(e)}
+            />
+            {filteredMonsters.map(monster => { return <div key={monster.id}>{monster.name}</div>})}
+        </div>
+    )
 }
 
 export default App
